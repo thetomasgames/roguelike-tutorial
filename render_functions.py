2bd3516c3a12fff355c77e78debfd20694996733
tomaslm <tomaslm@hotmail.com>
2020-06-21 13:57:18 -0300

    Part 4: Inclusion of fov

--- render_functions.py
+++ render_functions.py
@@ -3,18 +3,61 @@ import tcod as libtcod
 
 def _draw_entity(con, entity):
     libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+    libtcod.console_put_char(
+        con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE
+    )
 
 
-def render_all(con, entities, game_map, screen_width, screen_height, colors):
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = game_map.tiles[x][y].block_sight
+def render_all(
+    con,
+    entities,
+    game_map,
+    fov_map,
+    fov_recompute,
+    screen_width,
+    screen_height,
+    colors,
+):
+    if fov_recompute:
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
 
-            if wall:
-                libtcod.console_set_char_background(con, x, y, colors.get("dark_wall"), libtcod.BKGND_SET)
-            else:
-                libtcod.console_set_char_background(con, x, y, colors.get("dark_ground"), libtcod.BKGND_SET)
+                if visible:
+                    if wall:
+                        libtcod.console_set_char_background(
+                            con,
+                            x,
+                            y,
+                            colors.get("light_wall"),
+                            libtcod.BKGND_SET,
+                        )
+                    else:
+                        libtcod.console_set_char_background(
+                            con,
+                            x,
+                            y,
+                            colors.get("light_ground"),
+                            libtcod.BKGND_SET,
+                        )
+                else:
+                    if wall:
+                        libtcod.console_set_char_background(
+                            con,
+                            x,
+                            y,
+                            colors.get("dark_wall"),
+                            libtcod.BKGND_SET,
+                        )
+                    else:
+                        libtcod.console_set_char_background(
+                            con,
+                            x,
+                            y,
+                            colors.get("dark_ground"),
+                            libtcod.BKGND_SET,
+                        )
 
     for entity in entities:
         _draw_entity(con, entity)
